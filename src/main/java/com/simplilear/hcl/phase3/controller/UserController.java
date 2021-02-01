092e58c089565bcd39599df115170b02c7dcab99
dmdijumathew22 <dmdijumathew22@gmail.com>
2021-02-01 12:48:30 -0500

    Logger added

--- src/main/java/com/simplilear/hcl/phase3/controller/UserController.java
+++ src/main/java/com/simplilear/hcl/phase3/controller/UserController.java
@@ -2,6 +2,8 @@ package com.simplilear.hcl.phase3.controller;
 
 import java.util.Optional;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -12,8 +14,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-import org.springframework.web.servlet.view.RedirectView;
+
 
 import com.simplilear.hcl.phase3.repository.UserEntity;
 import com.simplilear.hcl.phase3.services.UserService;
@@ -25,14 +26,18 @@ public class UserController {
 	UserService userService;
 	@Autowired
 	Optional<UserEntity> user;
+	
+	Logger logger = LoggerFactory.getLogger(UserController.class);
 
 	@GetMapping(path = "/")
 	String hello() {
+		logger.info("Mapping to index");
 		return "user/index";
 	}
 
 	@PostMapping(path = "/find")
 	String find(@RequestParam(name="userid") String userid, Model model) {
+		logger.info("post mapping /find");
 		user=userService.findById(userid);
 		if(user.isPresent()) {
 			System.out.println("here"+user.toString());
@@ -54,6 +59,7 @@ public class UserController {
 	@PostMapping(path ="/update")
 	String update (@ModelAttribute("employee")UserEntity user, 
 		      BindingResult result, ModelMap model) {
+		logger.info("in /update");
 		System.out.println("here update"+user.toString());
 			userService.update(user);	
 			model.addAttribute("msg", "User Information Updated");
